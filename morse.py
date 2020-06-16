9eb77b5bdf8c26b72dca305c00268003862637ae
Alakin <alakin@free.fr>
2020-06-16 19:04:48 +0200

    Morse code. Failing tests. Pending Jenkins configuration

---
+++ morse.py
@@ -0,0 +1,12 @@
+# pylint: disable=missing-docstring
+
+def decode(message):
+    morse_dict = {"/": " ", ".-": "A", "-...": "B", "-.-.": "C", "-..": "D", ".": "E", "..-.": "F", "--.": "G", "....": "H", "..": "I",".---": "J", "-.-": "K", ".-..": "L", "--": "M", "-.": "N", "---": "O", ".--.": "P", "--.-": "Q", ".-.": "R", "...": "S", "-": "T", "..-": "U", "...-": "V", ".--": "W", "-..-": "X", "-.--": "Y", "--..": "Z", "... --- ...": "SOS",}
+    morses = message.replace('/', '')
+    morses = message.split(" ")
+    result = ""
+    for key in morses:
+        result += morse_dict.get(key)
+    print(result)
+
+sentence = decode(".- .-.. .-.. / -.-- --- ..- / -. . . -.. / .. ... / -.-. --- -.. .")
